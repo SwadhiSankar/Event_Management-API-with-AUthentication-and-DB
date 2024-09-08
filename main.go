fb4961560ad63c60ed373bfef6b8e214b44a8d0d
SwadhiSankar <swadhis14@gmail.com>
2024-09-08 19:07:52 +0530

    basic get api

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 
 import "github.com/gin-gonic/gin"
+import "net/http"
 
 func main(){
   server :=gin.Default()
 
+  server.GET("/EVENTS",getEvents)  //GET, POST, PUT, PATCH, DELETE
+
   server.Run(":8080") //starts listening incoming request
+}
+
+func getEvents(context *gin.Context ) {
+ context.JSON(http.StatusOK,gin.H{"message": "Hello"})
 }
