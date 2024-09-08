9647ea1947411fb3c17ee07d5f1d969320c9d301
SwadhiSankar <swadhis14@gmail.com>
2024-09-08 20:04:21 +0530

    model class, added validation, adding post request and error validation

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
-package main 
+package main
 
+import (
+	"net/http"
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+	// "example.com/main/models"
+	"example.com/main.go/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main(){
   server :=gin.Default()
 
   server.GET("/EVENTS",getEvents)  //GET, POST, PUT, PATCH, DELETE
-
+  server.POST("/events",createEvent)
   server.Run(":8080") //starts listening incoming request
 }
 
 func getEvents(context *gin.Context ) {
- context.JSON(http.StatusOK,gin.H{"message": "Hello"})
+ events := models.GetAllEvent()
+ context.JSON(http.StatusOK,events)
+}
+
+func createEvent(context *gin.Context){
+ var event models.Event
+
+ err  := context.ShouldBindJSON(&event)
+
+ if err !=nil {
+ context.JSON(http.StatusBadRequest,gin.H{"message": "Missing required fields data"})
+ return
+ }
+ event.ID = 1
+ event.UserID = 1
+ context.JSON(http.StatusCreated,gin.H{"message":"Eventcreated","event": event})
 }
---
+++ models/event.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string `binding: "required"`
+	Description string `binding: "required"`
+	Location    string
+	DateTime    time.Time
+	UserID int
+
+}
+var events = []Event{}
+func(e Event) Save(){
+	//db code 
+   events= append(events, e)
+
+}
+
+func GetAllEvent() []Event{
+	return events
+
+}
