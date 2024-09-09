d02ccff5ef5a65e9023ecbe94e06a65a22d50d64
SwadhiSankar <swadhis14@gmail.com>
2024-09-09 22:19:57 +0530

    fixed gcc server error for sqlite3

--- db/db.go
+++ db/db.go
@@ -1,23 +1,27 @@
 package db
+
 import (
-   "database/sql"
- _ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 
 var DB *sql.DB
-var err error
-func InitDB(){
-	DB,err = sql.Open("sqlite3","api.db") //drivername, db name
-   if err !=nil{
-	panic("could not connect to db")
-   }
-
-   DB.SetMaxOpenConns(10) //no of conn can open (controlling ) 
-   DB.SetMaxIdleConns(5)
-
-   createTables()
-
+ 
+func InitDB() {
+    var err error
+    DB, err = sql.Open("sqlite3", "api.db")
+ 
+    if err != nil {
+        panic("Could not connect to database.")
+    }
+ 
+    DB.SetMaxOpenConns(10)
+    DB.SetMaxIdleConns(5)
+ 
+    createTables()
 }
 
 func createTables() {
@@ -33,6 +37,6 @@ func createTables() {
    `
    _, err := DB.Exec(creatEventTable)
    if err !=nil {
-      panic("Could not able to create Table")
+      panic(fmt.Sprintf("Could not create table: %v", err))
    }
 }
